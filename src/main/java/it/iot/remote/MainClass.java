4e91f8888b7890b0ca4bc1d206feb6a7b6e38c30
leobel99 <leobel99@gmail.com>
2023-07-31 13:45:08 +0200

    Inserimento secondo main (fare classe per comando da cli + inserire poi thread user nel secondo main)

--- src/main/java/it/iot/remote/MainClass.java
+++ src/main/java/it/iot/remote/MainClass.java
@@ -1,4 +1,18 @@
 package it.iot.remote;
 
+import it.iot.remote.server.Server;
+
+
 public class MainClass {
+    public static void main(String[] args) {
+        // start thread for registration server execution
+        Server server = new Server();
+        Thread th = new Thread(server);
+        th.start();
+
+        // start thread for user command listener
+        /*UserInputController userInputController = new UserInputController();
+        Thread userInputControllerThread = new Thread(userInputController);
+        userInputControllerThread.start();*/
+    }
 }
