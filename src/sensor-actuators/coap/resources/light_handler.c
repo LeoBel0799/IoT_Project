6addd835d817e80001e19f83ecee8cc635865c52
leobel99 <leobel99@gmail.com>
2023-08-22 10:44:17 +0200

    GET light (da fixare)

--- src/sensor-actuators/coap/resources/light_handler.c
+++ src/sensor-actuators/coap/resources/light_handler.c
@@ -11,16 +11,21 @@
 #define MAX_ARGS 10
 #define ARG_LEN 100
 extern uint8_t led;
+char light_mode[10];
 
+
+static void light_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void light_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
 RESOURCE(res_light_controller,
          "title=\"light controller\";rt=\"light\"",
-         NULL,
+         light_get_handler,
          NULL,
          light_put_handler,
          NULL);
 
 bool light_on = false;
+
 
 static void light_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
     size_t len = 0;
@@ -28,13 +33,21 @@ static void light_put_handler(coap_message_t *request, coap_message_t *response,
 
      if((len = coap_get_query_variable(request, "command", &command))){
          LOG_DBG("Command received is %.*s\n", (int)len, command);
+             if (len >= sizeof(light_mode)) {
+               LOG_ERR("Command too long");
+               return;
+             }
          if(strncmp(command, "ON", len) == 0){
             light_on = true;
             LOG_INFO("Light ON\n");
+            strcpy(light_mode,command);
+
          }else if (strncmp(command,"OFF",len) == 0){
             light_on = false;
              leds_off(LEDS_ALL);
              LOG_INFO("Light OFF\n");
+             strcpy(light_mode,command);
+
          }else{
             goto error;
          }
@@ -42,4 +55,23 @@ static void light_put_handler(coap_message_t *request, coap_message_t *response,
          error:
          	coap_set_status_code(response, BAD_REQUEST_4_00);
      }
+}
+
+static void light_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+
+    const char * status = light_mode;
+
+      char payload[100];
+      int len = snprintf(payload, sizeof(payload), "{\"lights\": \"%s\"}", status);
+
+      if(len < 0 || len >= sizeof(payload)) {
+        LOG_ERR("Payload too long");
+        return;
+      }
+
+      LOG_INFO("GET /actuator/light");
+      LOG_INFO(" >  %s\n", payload);
+      coap_set_header_content_format(response, APPLICATION_JSON);
+      coap_set_payload(response, buffer, len);
+
 }
