fc4d42c1efd94a4459ba14aa9e2c0354422eaae4
leobel99 <leobel99@gmail.com>
2023-08-24 19:37:26 +0200

    Aggiunta thread per esecuzione unica durante tutto il programma per il metodo che fa accendere luci in base alla tabella di MQTT

--- src/main/java/it/iot/application/user/UserMenu.java
+++ src/main/java/it/iot/application/user/UserMenu.java
@@ -8,9 +8,10 @@ import it.iot.application.controller.PoweringLights;
 import org.eclipse.californium.elements.exception.ConnectorException;
 
 import java.io.IOException;
-import java.net.SocketException;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class UserMenu implements Runnable {
 
@@ -79,38 +80,38 @@ public class UserMenu implements Runnable {
         while (!shouldExit){
             showMenu();
             //TESTATO TRY CATCH PER INPUTMISMATCH E FUNGE
+            //Il thread viene eseguito una volta sola per tutta la durata del programma cosi il primo comando l'attuatore lo fa in base a ciò che manda MQTT
+            //poi le azioni che fa l'utente sono tutte basate sulle sue azioni precedenti quindi basondosi sulle azioni fatte sull'attuatore
             try{
                 int choice = input.nextInt();
                 switch (choice) {
                     case OPTION_TURN_ON_LIGHT:
                         int lightId = askForLightId();
-                        if (lightData.lightExists(lightId)){
+                        final boolean[] setLightExecuted = {false};
+                        if (!setLightExecuted[0] && lightData.lightExists(lightId)) {
+                             final PoweringLights lights = new PoweringLights(lightId, lightData, nodeData, actuatorStatus);
+                            // Creazione di un pool di thread con un solo thread
+                            ExecutorService executor = Executors.newSingleThreadExecutor();
+                            // Esecuzione della funzione setLight() nel thread
+                            executor.submit(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        lights.setInitialStateofLight(); // Chiamata alla funzione setLight()
+                                        setLightExecuted[0] = true; // Imposta la variabile a true dopo l'esecuzione
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                            // Chiusura del pool di thread dopo che la funzione è stata eseguita
+                            executor.shutdown();
+                        } else if (setLightExecuted[0]) {
                             PoweringLights lights = new PoweringLights(lightId, lightData, nodeData, actuatorStatus);
-                            lights.setLight();
-                        }else{
+                            lights.setLightBasedOnActuatorsData(lightId);
+                        } else {
                             System.out.println("[INFO] - No Light status records with ID: " + lightId + ", still arrived!");
                         }
-
-
-                    /*
-                    SPIEGAZIONE DI COME è STATO IMPLMENTATO OBS, CON RELATIVO CAMBIO AUTOMATICO DEI VALORI SOTTO PRESSIONE DI UN BOTTONE
-                    Lato Java:
-                    Il codice Java effettua una richiesta GET CoAP al nodo C per ottenere i valori correnti di wearLevel e fulminated. Questi vengono ritornati in una stringa separata da virgola.
-                    Java elabora la risposta splittandola e convertendo wearLevel in float e fulminated in boolean.
-                    In base ai valori ottenuti, il codice decide se accendere/spegnere la luce e se resettare i valori.
-                    Dopo aver inviato il comando di accensione/spegnimento tramite una richiesta PUT CoAP, il codice rilegge lo stato corrente della luce con una nuova richiesta GET.
-                    I nuovi valori di stato, wearLevel e fulminated vengono salvati nel database tramite chiamate JDBC.
-                    Infine il codice invia i nuovi valori di wearLevel e fulminated come stringa al nodo C tramite una richiesta POST CoAP.
-                    Lato C:
-
-                    All'avvio, il nodo C espone una risorsa CoAP osservabile per wearLevel e fulminated.
-                    Alla richiesta GET Java, il gestore della risorsa crea la stringa di risposta con i valori correnti e la ritorna.
-                    Alla ricezione della richiesta POST Java, il gestore aggiorna le variabili globali wearLevel e fulminated.
-                    Alle successive richieste GET, i valori aggiornati verranno letti dalle variabili globali e ritornati a Java.
-                    Se viene premuto il pulsante, wearLevel e fulminated vengono resettati e la risorsa notifica Java tramite una osservazione CoAP.
-                    In sintesi, Java e C si scambiano i dati su wearLevel e fulminated tramite richieste CoAP. Java decide le azioni da intraprendere e salva lo storico nel DB. C mantiene i valori correnti in memoria e li notifica a Java quando cambiano.
-                    */
-                        //Per capire i dati se arrivano e come arrivano apriti una finestra con sql dove fai select* su tutte e tre le tabelle ogni tot di secondi cos' vedi gli inserimenti
                         break;
 
 
