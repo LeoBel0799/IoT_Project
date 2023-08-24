f50ab8186f2d25e7553a4333bb3048f1d4967e68
leobel99 <leobel99@gmail.com>
2023-08-24 19:31:20 +0200

    Aggiunta colonna counter per aggiornamento dello stesso + wear level "in live" + metodi DB di supporto

--- src/main/java/it/iot/application/DB/ActuatorStatus.java
+++ src/main/java/it/iot/application/DB/ActuatorStatus.java
@@ -5,10 +5,13 @@ import org.eclipse.californium.elements.exception.ConnectorException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ActuatorStatus {
     private DB db;
+    Map<Integer, Integer> actuatorCounters = new HashMap<>();
 
     public ActuatorStatus() throws ConnectorException, IOException {
         System.out.println("[INFO] - Connected to Collector DB, ready to insert Actuator status in DB|");
@@ -18,6 +21,7 @@ public class ActuatorStatus {
         String sql = "CREATE TABLE actuator (" +
                 "id INTEGER AUTO_INCREMENT  PRIMARY KEY, " +
                 "idActuator INTEGER, " +
+                "counter INTEGER,"+
                 "light VARCHAR(10), " +
                 "bright VARCHAR(10)," +
                 "wearLevel INTEGER, "+
@@ -46,14 +50,17 @@ public class ActuatorStatus {
         }
     }
 
-    public void insertActuatorData(int idActuator, String light, String bright, Double wearLevel, Boolean fulminated) throws SQLException {
-        String insert = "INSERT INTO actuator (idActuator,light,bright,wearLevel,fulminated) VALUES (?,?,?,?,?)";
+    public void insertActuatorData(int idActuator, int counter, String light, String bright, Double wearLevel, Boolean fulminated) throws SQLException {
+        String insert = "INSERT INTO actuator (idActuator, counter, light,bright,wearLevel,fulminated) VALUES (?,?,?,?,?,?)";
         System.out.println("[INFO] - Receiving actuator data");
+        int count = actuatorCounters.getOrDefault(idActuator, 0) + 1;
+        actuatorCounters.put(idActuator, count);
 
         try {
             Connection conn = db.connDb();
             PreparedStatement stmt = conn.prepareStatement(insert);
             stmt.setInt(1,idActuator);
+            stmt.setInt(2,count);
             stmt.setString(2,light);
             stmt.setString(3,bright);
             stmt.setDouble(4,wearLevel);
@@ -80,10 +87,11 @@ public class ActuatorStatus {
             while(rs.next()) {
                 String row = " ";
                 row += "IdActuator: " + rs.getInt("idActuator");
+                row += ", Counter: " + rs.getString("counter");
                 row += ", Lights: " + rs.getString("light");
                 row += ", Brights: " + rs.getString("bright");
                 row += ", Wear Level: " + rs.getInt("wearLevel");
-                row += ", Fulminated: " + rs.getString("fulminated");
+                row += ", Fulminated: " + rs.getBoolean("fulminated");
                 row += ", Time:" + rs.getString("created_at");
                 rows.add(row);
             }
@@ -98,35 +106,6 @@ public class ActuatorStatus {
     }
 
 
-    public String[] getActuatorData(int idLight) throws SQLException {
-            String[] results = new String[2];
-            String select = "SELECT wearLevel, fulminated FROM actuator WHERE idActuator=? ORDER BY created_at DESC LIMIT 1";
-
-            try {
-                Connection conn = db.connDb();
-                PreparedStatement stmt = conn.prepareStatement(select);
-                stmt.setInt(1, idLight);
-
-                ResultSet rs = stmt.executeQuery();
-                if (!rs.next()) {
-                    results[0] = "[INFO] - Still no records for this actuator. RETRY!";
-                    results[1] = "";
-                    return results;
-                }
-
-                String wearLevel = rs.getString("wearLevel");
-                String fulminated = rs.getString("fulminated");
-                results[0] = wearLevel;
-                results[1] = fulminated;
-
-            } catch (SQLException e) {
-                System.err.println("[FAIL] - Error during reading wearLevel and Fulminated data from DB\n");
-                e.printStackTrace(System.err);
-                e.getMessage();
-            }
-
-            return results;
-    }
 
     public String getLightStatusFromActuator(int idLight) throws SQLException {
 
@@ -154,12 +133,57 @@ public class ActuatorStatus {
 
     }
 
+    public Double getWearLevelromActuator(int idActuator) throws SQLException {
+
+        Double lightStatus = null;
+
+        String select = "SELECT wearLevel FROM actuator WHERE idActuator=? ORDER BY created_at DESC LIMIT 1";
+
+        try {
+
+            Connection conn = db.connDb();
+            PreparedStatement stmt = conn.prepareStatement(select);
+            stmt.setInt(1, idActuator);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                lightStatus = rs.getDouble("wearLevel");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error reading wear level from DB: " + e.getMessage());
+        }
+
+        return lightStatus;
+
+    }
 
 
+    public boolean setFulminatedStatus(int idActuator) throws SQLException {
+        String update = "UPDATE actuator SET fulminated = ? WHERE idActuator = ?";
+        try {
+            Connection conn = db.connDb();
+            PreparedStatement stmt = conn.prepareStatement(update);
+            stmt.setBoolean(1, true);
+            stmt.setInt(2, idActuator);
 
-    public void insertWearAndFulminatedResetted(int idActuator, float wear, boolean fulm) throws SQLException {
+            int rowsAffected = stmt.executeUpdate();
+            if (rowsAffected == 0) {
+            } else {
+                System.out.println("No rows affected. Actuator with ID not found: " + idActuator);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error updating fulminated status in DB: " + e.getMessage());
+        }
+        return true;
 
-        String update = "UPDATE actuator SET wearLevel = ?, fulminated = ? WHERE idActuator = ?";
+    }
+
+
+    public void insertWearAndFulminatedResetted(int idActuator, float wear, boolean fulm, int count) throws SQLException {
+
+        String update = "UPDATE actuator SET wearLevel = ?, fulminated = ?, counter = ? WHERE idActuator = ?";
 
         try {
 
@@ -169,6 +193,7 @@ public class ActuatorStatus {
 
             stmt.setFloat(1, wear);
             stmt.setBoolean(2, fulm);
+            stmt.setInt(3,count);
             stmt.setInt(3, idActuator);
 
             stmt.executeUpdate();
@@ -180,5 +205,31 @@ public class ActuatorStatus {
         }
     }
 
+    public int getCounterForActuator(int idActuator) {
+
+        int counter = 0;
+        try {
+            Connection conn = DB.connDb();
+            // Query per ottenere il contatore
+            String sql = "SELECT MAX(counter) AS counter FROM actuator WHERE idActuator = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, idActuator);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                // Estrai contatore
+                counter = rs.getInt("counter");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("[FAIL] - Error during Counter reading field from DB");
+            e.printStackTrace(System.err);
+            e.getMessage();
+        }
+
+        return counter;
+
+    }
 
 }
