0cf964136ba31ae3d963c497d74be7aa157faa95
leobel99 <leobel99@gmail.com>
2023-08-24 19:29:09 +0200

    Adeguamento PUT a quella di light_handler.c

--- src/sensor-actuators/coap/resources/bright_handler.c
+++ src/sensor-actuators/coap/resources/bright_handler.c
@@ -12,7 +12,7 @@
 #define LOG_LEVEL LOG_LEVEL_APP
 #define MAX_ARGS 10
 #define ARG_LEN 100
-bool bright_on = false;
+bool bright_on = 0;
 
 
 static void bright_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
@@ -26,42 +26,43 @@ RESOURCE(res_bright_controller,
          bright_put_handler,
          NULL);
 
-uint8_t led;
 
 static void bright_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
     size_t len = 0;
     const char *command = NULL;
-
+    uint8_t led = 0;
+    int success =1;
     if((len = coap_get_query_variable(request, "command", &command))){
-             LOG_DBG("Command received is %.*s\n", (int)len, command);
-             if(strncmp(command, "ON", len) == 0){
-                    leds_on(LEDS_YELLOW);
-                    bright_on = true;
-                    LOG_INFO("Bright ON");
-             }else if (strncmp(command,"OFF",len) == 0){
-                    bright_on = false;
-                    leds_off(LEDS_ALL);
-                    LOG_INFO("Bright OFF");
-             }else{
-                goto error;
-             }
-
-
-              LOG_INFO("Color = %s\n", command);
-              if(bright_on) {
-                  leds_set(led);
-              }
-             return;
-                error:
-             	    coap_set_status_code(response, BAD_REQUEST_4_00);
+        LOG_DBG("Command %.*s - WAIT!\n", (int)len, command);
+        // Spengo la luce
+        if(strncmp(command, "OFF", len) == 0){
+            led = LEDS_RED;
+            bright_on = 0;
+            LOG_INFO("[OK] - Bright OFF");
+        }else if(strncmp(command, "ON", len) == 0){
+            led = LEDS_BLUE;
+            bright_on = 1;
+            LOG_INFO("[OK] - Bright ON");
+        }else{
+      coap_set_status_code(response, BAD_REQUEST_4_00);
     }
+  }else{
+    coap_set_status_code(response, BAD_REQUEST_4_00);
+  }
+
+  if(success) {
+    coap_set_status_code(response, CONTENT_2_05);
+    leds_off(LEDS_ALL);
+    leds_on(led);
+  }
+
 }
+
 
 
 static void bright_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
   int length = 0;
   const char* message;
-    usleep(100000);
   if (bright_on == true){
       length = 2;
       message = "ON";
